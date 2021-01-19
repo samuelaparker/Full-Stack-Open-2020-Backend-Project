7390954d72c093b639ce832e08f38fefd5fa0d7b
samuelaparker <samuel.a.parker00@gmail.com>
2021-01-18 19:16:56 -0500

    ex. 3.3

--- index.js
+++ index.js
@@ -2,45 +2,53 @@ const express = require('express')
 let app = express()
 
 let persons = [
-      { 
-        "name": "Arto Hellas", 
+    {
+        "name": "Arto Hellas",
         "number": "040-123456",
         "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
+    },
+    {
+        "name": "Ada Lovelace",
         "number": "39-44-5323523",
         "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
+    },
+    {
+        "name": "Dan Abramov",
         "number": "12-43-234345",
         "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
+    },
+    {
+        "name": "Mary Poppendieck",
         "number": "39-23-6423122",
         "id": 4
-      }
-    ]
+    }
+]
 
 let date = new Date();
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
-
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
-
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people. <br>${date}</p>` )
-    
+    response.send(`<p>Phonebook has info for ${persons.length} people. <br>${date}</p>`)
+
 })
-  
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
