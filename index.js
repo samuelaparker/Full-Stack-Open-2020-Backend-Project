4a92facb16df7787abef141ab0e6feceb912fe17
samuelaparker <samuel.a.parker00@gmail.com>
2021-01-29 13:59:55 -0500

    ex. 3.14

--- index.js
+++ index.js
@@ -122,8 +122,6 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
